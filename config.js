b0b63fe0ab5213e36ca5f4cece91eba0df5029ac
Dan Ingamells <dan_ingamells@hotmail.co.uk>
2018-09-17 13:52:29 +0100

    .gitignore is now working

--- config.js
+++
@@ -1,6 +0,0 @@
-const token = module.exports = {
-    consumer_key:         "TArEXLlIrMpxQlUzQyoRZEqq7"
-  , consumer_secret:      "7XnMgK6aKTe32BkPjgM2ghQD1lll6X7zcwoUEtdqqmZGG4RVKz"
-  , access_token:         "439346646-C5HME1NzoHCIPLfkrU4CoRApzvwrH30GNlc31YRJ"
-  , access_token_secret:  "KQWTiS31c4hfZiB2ZOP7cUJD9YE8gJ4bChm0AmDkV2hjb"
-};
